c85f387828d24dd0eb0421021f9648e775c70ae6
mali295 <mali295@uwo.ca>
2022-12-28 12:22:23 -0500

    Complete context for google and facebook

---
+++ src/components/Chats.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { ChatEngine } from 'react-chat-engine'
+import { auth } from '../firebase'
+
+
+const Chats = () => {
+    const history = useHistory()
+    const handleLogout = async () => {
+        await auth.signOut()
+
+        history.push('/')
+        
+    }
+
+    return (
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Unichat
+                </div>
+                <div onClick={handleLogout} className="logout-tab">
+                    Logout
+                </div>
+            </div>
+
+            <ChatEngine
+                height="calc(100vh - 66px)"
+                projectId="6569d195-2778-4945-9992-b96447b15b42"
+                userName="."
+                userSecret="."
+            />
+        </div>
+    )
+}
